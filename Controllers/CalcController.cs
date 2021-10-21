194fead78cdfffa0967316b8c536f48b7e9d825f
Max <yashanty@gmail.com>
2021-10-22 01:09:43 +0300

    Add project files.

---
+++ Controllers/CalcController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace asp_less3_ex1_calc.Controllers
+{
+    public class CalcController : Controller
+    {
+        public IActionResult Index(int x=0, int y=0, string sub=null)
+        {
+            switch (sub)
+            {
+                case " + ":
+                    ViewBag.Result = x + y;
+                    break;
+                case " - ":
+                    ViewBag.Result = x - y;
+                    break;
+                case " * ":
+                    ViewBag.Result = x * y;
+                    break;
+                case " / ":
+                    {
+                        if (y == 0)
+                        {
+                            ViewBag.Result = null;
+                            ViewBag.Commit = "На ноль делить нельзя!";
+                            break;
+                        }
+                        ViewBag.Result = x / y;
+                        break;
+                    }
+                default:
+                    ViewBag.Result = 0;
+                    break;
+            }
+            return View();
+        }
+    }
+}
